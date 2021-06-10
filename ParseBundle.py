892f5f4442834ee2a392a65cccad77ce0503c747
Michel Peters <michelpe@cisco.com>
2021-06-10 08:43:06 +0200

    - Adding license headers

--- ParseBundle.py
+++ ParseBundle.py
@@ -1,3 +1,19 @@
+"""Copyright (c) 2021 Cisco and/or its affiliates.
+
+This software is licensed to you under the terms of the Cisco Sample
+Code License, Version 1.1 (the "License"). You may obtain a copy of the
+License at
+
+               https://developer.cisco.com/docs/licenses
+
+All use of the material herein must be in accordance with the terms of
+the License. All rights not expressly granted by the License are
+reserved. Unless required by applicable law or agreed to separately in
+writing, software distributed under the License is distributed on an "AS
+IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+or implied.
+"""
+
 import sys
 import getopt
 import os
@@ -108,6 +124,7 @@ def ParseBundle(dnac_core,dir):
     Analysis.CheckAuth(dnac,dnac_core)
     Analysis.CheckRLOCreach(dnac,dnac_core)
     Analysis.CheckCTS(dnac,dnac_core)
+    #Analysis.checksvi(dnac,dnac_core)
     #dnac_core.printit()
     return
 
